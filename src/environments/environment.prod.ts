d2da13be33e58234a5be91e1a346823ab5b93818
ITAGH-CODE <i.taghbalout@gmail.com>
2020-05-14 14:29:13 +0000

    Change environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDaJuKx4lw0-JivNkYEFdVYdvdBzEi0JeA",
+    authDomain: "todolistapp-f0378.firebaseapp.com",
+    databaseURL: "https://todolistapp-f0378.firebaseio.com",
+    projectId: "todolistapp-f0378",
+    storageBucket: "todolistapp-f0378.appspot.com",
+    messagingSenderId: "57537651856",
+    appId: "1:57537651856:web:0a8f63cb33cb56b1cb6415",
+    measurementId: "G-TWKSR34HYC"
+  }
 };
